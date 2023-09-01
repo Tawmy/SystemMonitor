7f46eef3c71c6109d1e7b94854f4990fbd747a6f
Tawmy <git@tawmy.dev>
2023-09-01 08:44:13 +0200

    Add Dockerfile

--- SystemMonitor/EnvironmentVariables.cs
+++ SystemMonitor/EnvironmentVariables.cs
@@ -3,33 +3,53 @@
 public static class EnvironmentVariables
 {
     /// <summary>
-    ///     Used for JWT Token validation.
+    ///     Used for JWT Token validation. Optional.
     /// </summary>
+    /// <remarks>
+    /// If this and <see cref="AuthAuthority"/> are set, client authentication is enabled.
+    /// </remarks>
     public const string AuthAudience = "AUTH_AUDIENCE";
 
     /// <summary>
-    ///     Used for JWT Token validation.
+    ///     Used for JWT Token validation. Optional.
     /// </summary>
+    /// <remarks>
+    /// If this and <see cref="AuthAudience"/> are set, client authentication is enabled.
+    /// </remarks>
     public const string AuthAuthority = "AUTH_AUTHORITY";
 
     /// <summary>
     ///     OIDC Client ID for Swagger UI. Optional.
     /// </summary>
+    /// <remarks>
+    /// Only in effect if <see cref="SwaggerClientId"/> and <see cref="SwaggerClientSecret"/> are set.
+    /// </remarks>
     public const string SwaggerClientId = "SWAGGER_CLIENT_ID";
 
     /// <summary>
     ///     OIDC Client Secret for Swagger UI. Optional.
     /// </summary>
+    /// <remarks>
+    ///     Only in effect if <see cref="SwaggerClientId"/> and <see cref="SwaggerClientSecret"/> are set.
+    ///     <br/><br/>
+    ///     Do not set this if Swagger is available outside of your network.
+    /// </remarks>
     public const string SwaggerClientSecret = "SWAGGER_CLIENT_SECRET";
 
     /// <summary>
-    ///     OIDC auth endpoint URL.
+    ///     OIDC auth endpoint URL. Optional.
     /// </summary>
+    /// <remarks>
+    /// If this and <see cref="SwaggerUrlToken"/> are set, Swagger will show the "authorize" button.
+    /// </remarks>
     public const string SwaggerUrlAuth = "SWAGGER_URL_AUTH";
 
     /// <summary>
-    ///     OIDC token endpoint URL.
+    ///     OIDC token endpoint URL. Optional.
     /// </summary>
+    /// <remarks>
+    /// If this and <see cref="SwaggerUrlAuth"/> are set, Swagger will show the "authorize" button.
+    /// </remarks>
     public const string SwaggerUrlToken = "SWAGGER_URL_TOKEN";
 
     /// <summary>
